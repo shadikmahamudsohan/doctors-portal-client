47278162eae084659a33c14cb71ede05257747e1
shadikmahamud <shadikmahamudsohan@gmail.com>
2022-05-10 01:17:56 +0600

    banner and items section added

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from './Banner';
+import Info from './Info';
 
 const Home = () => {
     return (
         <div>
-            <h2>This is home</h2>
+            <Banner></Banner>
+            <Info></Info>
         </div>
     );
 };
