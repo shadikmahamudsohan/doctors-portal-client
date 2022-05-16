07e08a9f1b793f721c87bc96fe182ae9ea77c1fd
shadikmahamud <shadikmahamudsohan@gmail.com>
2022-05-17 01:04:50 +0600

    making add doctor page

--- src/Pages/Dashboard/MyAppointments.js
+++ src/Pages/Dashboard/MyAppointments.js
@@ -3,45 +3,39 @@ import React, { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
-import Loading from '../Shared/Loading';
 
 const MyAppointments = () => {
+    const [appointments, setAppointments] = useState([]);
     const [user] = useAuthState(auth);
-    const [appointments, setAppointments] = useState()
-    const [loading, setLoading] = useState(true)
     const navigate = useNavigate()
 
     useEffect(() => {
         if (user) {
-            setLoading(true)
-            fetch(`https://floating-everglades-70004.herokuapp.com/booking?patient=${user.email}`, {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`, {
                 method: 'GET',
                 headers: {
                     'authorization': `Bearer ${localStorage.getItem('accessToken')}`
-                },
+                }
             })
                 .then(res => {
                     console.log('res', res);
                     if (res.status === 401 || res.status === 403) {
+                        signOut(auth);
+                        localStorage.removeItem('accessToken');
                         navigate('/');
-                        signOut(auth)
-                        localStorage.removeItem('accessToken')
                     }
                     return res.json()
                 })
                 .then(data => {
-                    setAppointments(data)
-                    setLoading(false)
-                })
-        }
-    }, [user, navigate])
 
-    if (loading) {
-        return <Loading></Loading>
-    }
+                    setAppointments(data);
+                });
+        }
+    }, [user])
+
     return (
         <div>
-            <h2>My Appointments: {appointments?.length}</h2>
+            <h2>My Appointments: {appointments.length}</h2>
             <div class="overflow-x-auto">
                 <table class="table w-full">
                     <thead>
@@ -55,14 +49,15 @@ const MyAppointments = () => {
                     </thead>
                     <tbody>
                         {
-                            appointments?.map((a, index) => <tr key={index}>
-                                <td>{index + 1}</td>
+                            appointments.map((a, index) => <tr>
+                                <th>{index + 1}</th>
                                 <td>{a.patientName}</td>
                                 <td>{a.date}</td>
                                 <td>{a.slot}</td>
                                 <td>{a.treatment}</td>
                             </tr>)
                         }
+
 
                     </tbody>
                 </table>
